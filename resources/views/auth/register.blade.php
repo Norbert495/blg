b9db68b647412c6be4b7aa2ecd77c213a480c7eb
Norbert Borkowski <norbertborkowski@MacBook-Pro-Norbert.local>
2020-03-09 18:32:31 +0100

    Register/login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
-@section('title', 'Posts')
+@section('title', 'Register')
+
 
 @section('content')
 <div class="wrapper">
@@ -10,16 +11,16 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
         <div class="form-fieldset">
-            <input class="form-field" type="email" name="email" placeholder="Your e-mail">
+        <input class="form-field {{ $errors->has('email')  ? 'is-invalid' : ''}} " type="email" name="email" placeholder="Your e-mail" value={{ old('email') }}>
         </div>
         <div class="form-fieldset">
-            <input class="form-field" type="text" name="name" placeholder="Your name">
+            <input class="form-field {{ $errors->has('name')  ? 'is-invalid' : ''}} " type="text" name="name" placeholder="Your name" value={{ old('name') }}>
         </div>
         <div class="form-fieldset">
-            <input class="form-field" type="password" name="password" placeholder="Password">
+            <input class="form-field {{ $errors->has('password')  ? 'is-invalid' : ''}}" type="password" name="password" placeholder="Password">
         </div>
         <div class="form-fieldset">
-            <input class="form-field" type="password" name="password_confirmation" placeholder="Repeat password">
+            <input class="form-field " type="password" name="password_confirmation" placeholder="Repeat password">
         </div>
         <button class="button">Submit</button>
     </form>
